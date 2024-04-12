9f7107805a2f01ec8b51a796424f9b1cf2733245
Ziyuan Qin <ziyuan.qin@databricks.com>
2024-04-11 22:55:05 -0700

    add unit test for sql_migrate_external_hiveserde_in_place

--- src/databricks/labs/ucx/hive_metastore/table_migrate.py
+++ src/databricks/labs/ucx/hive_metastore/table_migrate.py
@@ -151,7 +151,7 @@ class TablesMigrator:
             return self._migrate_dbfs_root_table(src_table.src, src_table.rule, grants)
         if src_table.src.what == What.EXTERNAL_SYNC:
             return self._migrate_external_table(src_table.src, src_table.rule, grants)
-        if src_table.src.what == What.EXTERNAL_NO_SYNC_HIVESERDE:
+        if src_table.src.what == What.EXTERNAL_HIVESERDE:
             return self._migrate_external_table_hiveserde(
                 src_table.src, src_table.rule, grants, hiveserde_in_place_migrate
             )
